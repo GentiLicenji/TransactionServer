774f268af06d28051e5e1553bdd21763562e3cd7
Your Name <you@example.com>
2025-02-21 16:48:49 +0100

    [Sisal-02]: Added H2 datasource to test config.

--- src/test/java/com/sisal/transaction/server/util/TestConfig.java
+++ src/test/java/com/sisal/transaction/server/util/TestConfig.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 public class TestConfig {
@@ -14,5 +18,12 @@ public class TestConfig {
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
     }
 }
