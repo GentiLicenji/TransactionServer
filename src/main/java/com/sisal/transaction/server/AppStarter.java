102332d0f779e3a29557caa5fd6930f76604806a
Your Name <you@example.com>
2025-02-21 10:33:29 +0100

    [Sisal-01]:Reverted custom property name due to startup issues. [Sisal-03]: Added annotations for repository and db entities to be scanned.

--- src/main/java/com/sisal/transaction/server/AppStarter.java
+++ src/main/java/com/sisal/transaction/server/AppStarter.java
@@ -2,38 +2,26 @@ package com.sisal.transaction.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
+        "com.sisal.transaction.server.config",
         "com.sisal.transaction.server.test.controller",
         "com.sisal.transaction.server.controller",
-        "com.sisal.transaction.server.model.rest",
         "com.sisal.transaction.server.delegate",
-        "com.sisal.transaction.server.config",
+        "com.sisal.transaction.server.service",
+        "com.sisal.transaction.server.model.rest",
+        "com.sisal.transaction.server.model.db",
         "com.sisal.transaction.server.exception"})
+@EntityScan("com.sisal.transaction.server.model.db")
+@EnableJpaRepositories("com.sisal.transaction.server.repository")
 public class AppStarter extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AppStarter.class, args);
-    }
-
-    /**
-     * Customizes the name of the property file loaded.
-     *
-     * @param application
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        Map<String, Object> props = new HashMap<>();
-        props.put("spring.config.name", "transaction.spring");
-        application.properties(props);
-        return application;
     }
 }
