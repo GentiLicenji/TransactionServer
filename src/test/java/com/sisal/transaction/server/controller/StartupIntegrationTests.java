e16a9df6c78f5de1697449f81255f6355840194a
Your Name <you@example.com>
2025-02-23 16:52:41 +0100

    [SISAL-01]: Added basic healthCheck startup Integration Test.

---
+++ src/test/java/com/sisal/transaction/server/controller/StartupIntegrationTests.java
@@ -0,0 +1,37 @@
+package com.sisal.transaction.server.controller;
+
+import com.sisal.transaction.server.util.TestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Test Suite will test the Transaction API controller by ignoring the filter chain.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(TestConfig.class)
+@ActiveProfiles("test") // Loads application-test.properties
+public class StartupIntegrationTests {
+
+    private static final String PATH = "/actuator/health";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void whenAppContextStartsUP_thenReturnUpStatus() throws Exception {
+
+        mockMvc.perform(get(PATH))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP"));
+    }
+}
