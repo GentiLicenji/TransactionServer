dd6a640af910ddaf3515d24629a7ed1d4dd5461f
Your Name <you@example.com>
2025-02-25 16:08:02 +0100

    [SISAL-04]: Authentication. Created ApiKeyProperties for custom authentication feature. Adjusted the prop files with new properties.

---
+++ src/main/java/com/sisal/transaction/server/config/ApiKeyProperties.java
@@ -0,0 +1,73 @@
+package com.sisal.transaction.server.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Auth property config.
+ */
+
+@Component
+@ConfigurationProperties(prefix = "api.security")
+public class ApiKeyProperties {
+    private List<ApiClient> clients = new ArrayList<>();
+
+    public List<ApiClient> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<ApiClient> clients) {
+        this.clients = clients;
+    }
+
+    public ApiClient getClientByApiKey(String apiKey) {
+        return clients.stream()
+                .filter(client -> client.getApiKey().equals(apiKey))
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Inner class to map properties
+    public static class ApiClient {
+        private String name;
+        private String apiKey;
+        private String secretKey;
+        private List<String> roles = new ArrayList<>();
+
+        // Getters and setters
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getApiKey() {
+            return apiKey;
+        }
+
+        public void setApiKey(String apiKey) {
+            this.apiKey = apiKey;
+        }
+
+        public String getSecretKey() {
+            return secretKey;
+        }
+
+        public void setSecretKey(String secretKey) {
+            this.secretKey = secretKey;
+        }
+
+        public List<String> getRoles() {
+            return roles;
+        }
+
+        public void setRoles(List<String> roles) {
+            this.roles = roles;
+        }
+    }
+}
